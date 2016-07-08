9dcdce70f46344ae1e840a8ec7ffadc564696b67
fenix230 <fenix230@126.com>
2016-07-08 21:55:09 +0800

    123

--- GsResource.h
+++ GsResource.h
@@ -9,50 +9,6 @@
 SHAKURAS_BEGIN;
 
 
-//primitive资源
-template<class Vert>
-void GenerateCube(std::vector<Vert>& verts, std::vector<std::array<int, 3> >& itris) {
-	static Vert mesh[8]{
-		{ { 1, -1,  1, 1 } },
-		{ { -1, -1,  1, 1 } },
-		{ { -1,  1,  1, 1 } },
-		{ { 1,  1,  1, 1 } },
-		{ { 1, -1, -1, 1 } },
-		{ { -1, -1, -1, 1 } },
-		{ { -1,  1, -1, 1 } },
-		{ { 1,  1, -1, 1 } },
-	};
-
-	auto draw_plane = [&](int a, int b, int c, int d) {
-		Vert p1 = mesh[a], p2 = mesh[b], p3 = mesh[c], p4 = mesh[d];
-
-		p1.attrib.set(0, 0);
-		p2.attrib.set(0, 1);
-		p3.attrib.set(1, 1);
-		p4.attrib.set(1, 0);
-
-		int index = verts.size();
-		verts.push_back(p1);
-		verts.push_back(p2);
-		verts.push_back(p3);
-		itris.push_back({ index, index + 1, index + 2 });
-
-		index = verts.size();
-		verts.push_back(p3);
-		verts.push_back(p4);
-		verts.push_back(p1);
-		itris.push_back({ index, index + 1, index + 2 });
-	};
-
-	draw_plane(0, 1, 2, 3);
-	draw_plane(4, 5, 6, 7);
-	draw_plane(0, 4, 5, 1);
-	draw_plane(1, 5, 6, 2);
-	draw_plane(2, 6, 7, 3);
-	draw_plane(3, 7, 4, 0);
-}
-
-
 //texture资源
 GsTextureU32Ptr GridTexture();
 GsTextureU32Ptr LoadTexture(std::string filepath);
--- Shakuras.cpp
+++ Shakuras.cpp
@@ -16,10 +16,59 @@ using namespace shakuras;
 
 namespace skexample {
 
-	typedef std::tuple<GsTextureU32Ptr, Matrix44f, Matrix44f> Uniform;
-	typedef GsVertex<Vector2f, 2> Vertex;
-	typedef GsFragment<2> Fragment;
+	typedef std::tuple<GsTextureU32Ptr, Matrix44f, Matrix44f, Vector3f> Uniform;
+	typedef std::tuple<Vector2f, Vector4f> Attrib;
+	typedef GsVertex<Attrib, 5> Vertex;
+	typedef GsFragment<5> Fragment;
 	typedef GsStageBuffer<Uniform, Vertex> StageBuffer;
+
+	void GenerateCube(std::vector<Vertex>& verts, std::vector<std::array<int, 3> >& itris) {
+		static Vertex mesh[8]{
+			{ { 1, -1,  1, 1 } },
+			{ { -1, -1,  1, 1 } },
+			{ { -1,  1,  1, 1 } },
+			{ { 1,  1,  1, 1 } },
+			{ { 1, -1, -1, 1 } },
+			{ { -1, -1, -1, 1 } },
+			{ { -1,  1, -1, 1 } },
+			{ { 1,  1, -1, 1 } },
+		};
+
+		auto draw_plane = [&](int a, int b, int c, int d) {
+			Vertex p1 = mesh[a], p2 = mesh[b], p3 = mesh[c], p4 = mesh[d];
+
+			std::get<0>(p1.attrib).set(0, 0);
+			std::get<0>(p2.attrib).set(0, 1);
+			std::get<0>(p3.attrib).set(1, 1);
+			std::get<0>(p4.attrib).set(1, 0);
+
+			Vector4f norm = CrossProduct(p3.pos - p2.pos, p1.pos - p2.pos);
+			norm.w = 0.0f;
+			std::get<1>(p1.attrib) = norm;
+			std::get<1>(p2.attrib) = norm;
+			std::get<1>(p3.attrib) = norm;
+			std::get<1>(p4.attrib) = norm;
+
+			int index = verts.size();
+			verts.push_back(p1);
+			verts.push_back(p2);
+			verts.push_back(p3);
+			itris.push_back({ index, index + 1, index + 2 });
+
+			index = verts.size();
+			verts.push_back(p3);
+			verts.push_back(p4);
+			verts.push_back(p1);
+			itris.push_back({ index, index + 1, index + 2 });
+		};
+
+		draw_plane(0, 1, 2, 3);
+		draw_plane(4, 5, 6, 7);
+		draw_plane(0, 4, 5, 1);
+		draw_plane(1, 5, 6, 2);
+		draw_plane(2, 6, 7, 3);
+		draw_plane(3, 7, 4, 0);
+	}
 
 	class AppStage {
 	public:
@@ -30,6 +79,8 @@ namespace skexample {
 			GenerateCube(output_.vertlist, output_.itris);
 			std::get<0>(output_.uniform) = LoadTexture("1.png");
 			std::get<1>(output_.uniform) = Matrix44f::Perspective(kGSPI * 0.5f, w / h, 1.0f, 500.0f);
+			std::get<3>(output_.uniform).set(1, 1, 1);
+			Normalize(std::get<3>(output_.uniform));
 
 			alpha_ = 1;
 			pos_ = 3.5;
@@ -62,9 +113,16 @@ namespace skexample {
 	public:
 		void process(const Uniform& u, Vertex& v) {
 			const Matrix44f& mvtrsf = std::get<2>(u);
+
 			v.pos = mvtrsf.transform(v.pos);
-			v.varying[0] = v.attrib.x;
-			v.varying[1] = v.attrib.y;
+
+			v.varying[0] = std::get<0>(v.attrib).x;
+			v.varying[1] = std::get<0>(v.attrib).y;
+
+			Vector4f norm = mvtrsf.transform(std::get<1>(v.attrib));
+			v.varying[2] = norm.x;
+			v.varying[3] = norm.y;
+			v.varying[4] = norm.z;
 		}
 	};
 
