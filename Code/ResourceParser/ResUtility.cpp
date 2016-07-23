79d24fb636187e7428e506e4e90c3cc2054c925e
fenix230 <fenix230@126.com>
2016-07-23 13:52:33 +0800

    123

---
+++ Code/ResourceParser/ResUtility.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include "ResUtility.h"
+#include <fstream>
+#include <filesystem>
+
+
+_FSPFX path g_resourcedir;
+
+
+std::string& TrimSlash(std::string& str) {
+	while (!str.empty() && (str.back() == '/' || str.back() == '\\')) {
+		str.pop_back();
+	}
+
+	while (!str.empty() && (str.front() == '/' || str.front() == '\\')) {
+		str = str.substr(1);
+	}
+
+	return str;
+}
+
+
+void LoadResourceDir(std::string cfg_fname) {
+	std::ifstream fs(cfg_fname);
+	if (fs) {
+		std::string str;
+		std::getline(fs, str);
+		TrimSlash(str);
+
+		_FSPFX path reldir = str;
+		g_resourcedir = _FSPFX absolute(reldir);
+	}
+}
+
+
+_FSPFX path ResourceDir() {
+	return g_resourcedir;
+}
---
+++ Code/ResourceParser/ResUtility.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include <filesystem>
+
+
+#ifdef _RESPARSER_DLL
+#    define RESPARSER_DLL   __declspec(dllexport)
+#else
+#    define RESPARSER_DLL   __declspec(dllimport)
+#endif
+
+
+void LoadResourceDir(std::string cfg_fname = "ResourceDir.cfg");
+
+
+_FSPFX path ResourceDir();
