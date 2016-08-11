2731c75a2f33d3946c63792a9964388956ebf0a4
fenix230 <fenix230@126.com>
2016-08-11 16:04:31 +0800

    123

--- Code/OpenGLRenderer/GlDrawCall.h
+++ Code/OpenGLRenderer/GlDrawCall.h
@@ -1,15 +1,21 @@
 #pragma once
+#include "GlRendererDll.h"
 #include "GlBatch.h"
 #include "GlProgram.h"
 
 
+#pragma warning(push)
+#pragma warning(disable:4251)
+
 SHAKURAS_BEGIN;
 
 
-struct GlDrawCall {
+struct GLRENDERER_DLL GlDrawCall {
 	GlAbsProgramPtr program;
 	std::vector<GlBatchPtr> batchs;
 };
 
 
 SHAKURAS_END;
+
+#pragma warning(pop)
